16486519059da29c4d12e4c2035d7b1bc4afe685
kenya6565 <ken.australia0605@gmail.com>
2022-11-23 23:18:17 +0900

    prepare for redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// actions -> increment, decrement
+// typeはアクション名
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// reducer -> アクションと前の状態を組み合わせて新しい状態を作る
+const counterReducer = (state = 0, action) => {
+  // dispatchから発せられたactionに応じてstateを変える
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+// store
+let store = createStore(counterReducer);
+
+// subscribeで新しい状態を感知してログとして出力する
+store.subscribe(() => console.log(store.getState()))
+
+// dispatch
+// incrementというアクションをstoreに通知
+store.dispatch(increment())
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
