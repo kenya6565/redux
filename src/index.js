c6d4b2f43dfbcd3be58dcf4d27d570ae835ad2e3
fujisakikenya <fujisaki.k@dentsudigital.co.jp>
2022-11-24 12:59:32 +0900

    create Reducers

--- src/index.js
+++ src/index.js
@@ -4,40 +4,40 @@ import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
 
-// actions -> increment, decrement
-// typeはアクション名
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
+// // actions -> increment, decrement
+// // typeはアクション名
+// const increment = () => {
+//   return {
+//     type: "INCREMENT",
+//   };
+// };
 
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT",
+//   };
+// };
 
-// reducer -> アクションと前の状態を組み合わせて新しい状態を作る
-const counterReducer = (state = 0, action) => {
-  // dispatchから発せられたactionに応じてstateを変える
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-};
+// // reducer -> アクションと前の状態を組み合わせて新しい状態を作る
+// const counterReducer = (state = 0, action) => {
+//   // dispatchから発せられたactionに応じてstateを変える
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//   }
+// };
 
-// store
-let store = createStore(counterReducer);
+// // store
+// let store = createStore(counterReducer);
 
-// subscribeで新しい状態を感知してログとして出力する
-store.subscribe(() => console.log(store.getState()))
+// // subscribeで新しい状態を感知してログとして出力する
+// store.subscribe(() => console.log(store.getState()))
 
-// dispatch
-// incrementというアクションをstoreに通知
-store.dispatch(increment())
+// // dispatch
+// // incrementというアクションをstoreに通知
+// store.dispatch(increment())
 
 
 
