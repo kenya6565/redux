9956699b97315350338ebe69d208a55e55f441cb
kenya6565 <ken.australia0605@gmail.com>
2022-11-25 03:35:49 +0900

    set up Provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
 import allReducers from "./reducers";
+import { Provider } from "react-redux";
 
 const store = createStore(
   allReducers,
@@ -13,6 +14,9 @@ const store = createStore(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* ProviderでApp.tsxを囲むことで全てのファイルでstoreが使えるようになる→stateも全てのファイルで使える*/}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
