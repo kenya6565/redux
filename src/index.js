db337470ae19351dafa724b8690e6316a6dc0d1c
kenya6565 <ken.australia0605@gmail.com>
2022-11-25 03:19:30 +0900

    introduce redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { combineReducers, createStore } from "redux";
+import { createStore } from "redux";
+import allReducers from "./reducers";
 
-const store = createStore(combineReducers);
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
