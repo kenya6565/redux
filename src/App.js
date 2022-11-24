012949a88effde22d4f9283d582e750e16e629fa
kenya6565 <ken.australia0605@gmail.com>
2022-11-25 03:44:15 +0900

    add useSelector

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
+  // useSelectorを通じてstore内のcounter stateにアクセス
+  const counter = useSelector((state) => state.counter);
+  const isLogin = useSelector((state) => state.isLogin);
   return (
     <div className="App">
       <h1>Hello Redux</h1>
+      <h3>カウント: {counter}</h3>
+      {isLogin ? <h3>ログインに成功!</h3> : <h3>ログインして下さい</h3>}
     </div>
   );
 }
