cb47e7b0818ec3bd5a1b022ad383c9313f93e452
kenya6565 <ken.australia0605@gmail.com>
2022-11-25 04:00:06 +0900

    add action func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+import { increment } from "./actions";
 
 function App() {
   // useSelectorを通じてstore内のcounter stateにアクセス
@@ -11,7 +12,7 @@ function App() {
     <div className="App">
       <h1>Hello Redux</h1>
       <h3>カウント: {counter}</h3>
-      <button onClick={() => dispatch}>プラス</button>
+      <button onClick={() => dispatch(increment())}>プラス</button>
       {isLogin ? <h3>ログインに成功!</h3> : <h3>ログインして下さい</h3>}
     </div>
   );
