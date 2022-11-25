e8835f5a2cbf5516bcf7b6e092bed66021b2a9c1
fujisakikenya <fujisaki.k@dentsudigital.co.jp>
2022-11-25 14:14:42 +0900

    add login action

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
-import { increment } from "./actions";
+import { decrement, increment, login } from "./actions";
 
 function App() {
   // useSelectorを通じてstore内のcounter stateにアクセス
@@ -12,8 +12,11 @@ function App() {
     <div className="App">
       <h1>Hello Redux</h1>
       <h3>カウント: {counter}</h3>
-      <button onClick={() => dispatch(increment())}>プラス</button>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>−</button>
       {isLogin ? <h3>ログインに成功!</h3> : <h3>ログインして下さい</h3>}
+      <button onClick={() => dispatch(login())}>ログイン or ログアウト</button>
+
     </div>
   );
 }
